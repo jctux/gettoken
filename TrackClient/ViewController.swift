f974830efe2db715dc375478590f5fa677ac23f8
Juan Martinez <juan.martinez01@sap.com>
2016-05-31 17:55:15 -0500

    Previews to connect UI to Code

--- TrackClient/ViewController.swift
+++ TrackClient/ViewController.swift
@@ -12,7 +12,54 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let user = "admin"
+        
+        let pass = "admin"
+        
+        let requestURL: NSURL = NSURL(string: "http://io.arga-tracking.com:5180/android/request_token?username=\(user)@arga-tracking.com&password=\(pass)")!
+        
+        let urlRequest: NSMutableURLRequest = NSMutableURLRequest(URL: requestURL)
+        
+        let session = NSURLSession.sharedSession()
+        
+        let task = session.dataTaskWithRequest(urlRequest) {
+        
+            (data, response, error) -> Void in
+            
+            let httpResponse = response as! NSHTTPURLResponse
+            
+            let statusCode = httpResponse.statusCode
+            
+            if (statusCode == 200) {
+            
+                print("Everyone is fine, file downloaded successfully.")
+                
+                do{
+                    
+                    let json = try NSJSONSerialization.JSONObjectWithData(data!, options:.AllowFragments)
+                    
+                    if let result = json["result"] as? [[String: AnyObject]] {
+                
+                    }
+                    
+                    if let token = json["data"] as? [[String: AnyObject]]{
+                        print("token is:\(token)")
+                        someLabel.text = "\(someVar)"
+                        
+                    }
+                    
+                    
+                }catch {
+                    print("Error with Json: \(error)")
+                }
+                
+            }
+        }
+        
+        task.resume();
     }
 
     override func didReceiveMemoryWarning() {
